830f355cc244eed1a0437583f1cbadf28ec89b41
KFukushige65 <fukushigekeisuke@net77-dhcp139.sfc.keio.ac.jp>
2016-10-27 12:33:27 +0900

    練習問題7までの追加

--- scripts:main.js
+++ scripts:main.js
@@ -1,1 +1,38 @@
-ib
+function isEvenNumber(n){
+  if(n % 2 == 0){
+      return true;
+  }else{
+      return false;
+  }
+}
+
+ 
+ function toEvenNumber(number){
+    if(isEvenNumber(number)){
+        return number;
+    }
+    return number + 1;
+ }
+
+
+function toOddNumber(num){
+    if(isEvenNumber(num)){
+        return num + 1;
+    }
+    return num;
+}
+
+
+function sumN(number){
+    if(number >= 1){
+        let result = 0
+        return sum(number);
+    }
+    let result = 0;
+    let i = number;
+    while(i <= 1){
+    result = result + i;
+    i = i + 1;
+  }
+    return result;
+}
